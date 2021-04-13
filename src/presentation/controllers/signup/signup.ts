a43e079cc9008913bea657e983ee6ce6c5b5dda3
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-12 23:18:52 -0300

    refactor: move httpresponse 200 to ok helper

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { MissingParamsError, InvalidParamsError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse,EmailValidator, AddAccount } from './signup-protocols'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -31,10 +31,7 @@ export class SignUpController implements Controller {
         email,
         password
       })
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
