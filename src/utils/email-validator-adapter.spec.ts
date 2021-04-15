830898882bfba6ae1941d0fecb02f74d32cde3d7
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-14 21:53:09 -0300

    refactor: change email validator file name

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator'
-
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 // Mockar todas as dependências
 jest.mock('validator',() => ({
@@ -7,24 +6,26 @@ jest.mock('validator',() => ({
     return true
   }
 }))
-
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
 describe('Name of the group', () => {
   test('Sould return false if validator lib returs false',() => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator,'isEmail').mockReturnValueOnce(false)
 
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
   test('Sould return false if validator lib returs false',() => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
 
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   test('Sould call validator with correct data',() => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator,'isEmail')
 
     sut.isValid('any_email@mail.com')
