0e30bf37731887cc6b9cbfcafd4b465ef5867f9b
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-15 22:32:25 -0300

    refactor: ensure factories to create instances SUT tests

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,19 +1,39 @@
 import { Encrypter } from '@/data/protocols/Encrypter'
+import { AddAccount } from '@/domain/usecases/add-account'
 import { DbAddAccount } from './db-add-account'
+
+interface SutEncrypter{
+  sut: AddAccount
+  encryptStub: Encrypter
+}
+
+const makeEncryptStub = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return Promise.resolve('hashed_password')
+    }
+  }
+  const encryptStub = new EncrypterStub()
+  return encryptStub
+}
+
+const makeSut = (): SutEncrypter => {
+  const encryptStub = makeEncryptStub()
+  const sut = new DbAddAccount(encryptStub)
+  return {
+    encryptStub,
+    sut
+  }
+}
 
 describe('DbAddAccount', () => {
   test('Should call Encrypter with correct password',async () => {
-    class EncryptStub implements Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return Promise.resolve('hashed_password')
-      }
-    }
-    const encryptStub = new EncryptStub()
-    const sut = new DbAddAccount(encryptStub)
+    const { encryptStub, sut } = makeSut()
     const accountData = { name: 'valid_name',email: 'valid_email',password: 'valid_password' }
-    const encryptSpy = spyOn(encryptStub,'encrypt')
 
+    const encryptSpy = spyOn(encryptStub,'encrypt')
     await sut.add(accountData)
+
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 })
