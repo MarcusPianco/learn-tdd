981a85b06d6d60e21d275bac0347e94d210860ce
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-26 23:08:52 -0300

    feat: add mongo create connection helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+  async disconnect (): Promise<void> {
+    await this.clientMongo.close()
+  }
+}
