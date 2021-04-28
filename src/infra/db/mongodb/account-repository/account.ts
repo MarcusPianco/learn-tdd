fbd884ad870f9acda625516b1ddf94a7d85967d1
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-27 23:28:32 -0300

    refactor: ,pve mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,13 +2,12 @@ import { AddAccountRepository } from '@/data/protocols/add-account-repository'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccountDto } from '@/domain/usecases/add-account'
 
-import { MongoHelper } from '../helpers/mongo-helper'
+import { map, MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   public async add (accountDto: AddAccountDto): Promise<AccountModel> {
     const accountcollection = MongoHelper.getCollection('accounts')
     const resultOpInsertAccount = await accountcollection.insertOne(accountDto)
-    const { _id, ...accountWithoudUnderId } = resultOpInsertAccount.ops[0]
-    return Object.assign({},accountWithoudUnderId,{ id: _id })
+    return map(resultOpInsertAccount.ops[0])
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -15,4 +15,10 @@ export const MongoHelper = {
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
   }
+
+}
+
+export const map = (collection: any): any => {
+  const { _id, ...collectionWithoutId } = collection
+  return Object.assign({},collectionWithoutId,{ id: _id })
 }
